49b125d2f7159b4df852ba34c0261d0b67c06db4
stvking <tgfbikes@gmail.com>
2016-08-09 12:51:08 -0600

    advanced builds using es6 and alternate paths

---
+++ AdvancedBuilds/js/app.es6
@@ -0,0 +1,7 @@
+import {login} from './login'
+
+login('admin', 'things');
+
+document.write('Welcome to Webpack things and stuff');
+
+console.log('app loaded');
---
+++ AdvancedBuilds/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  context: path.resolve('js'),
+  entry: ['./utils', './app'],
+  output: {
+    path: path.resolve('build/js/'),
+    publicPath: '/public/assets/js/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'public'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.es6$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  }
+}
